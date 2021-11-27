1222975a7c0ae9346ec6f012bf2f749fa576e890
bhbs <bhbstar.me@gmail.com>
2021-11-27 17:43:06 +0900

    init socket io

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
+import { join } from 'path'
+import { createServer } from 'http'
+import { Server } from 'socket.io'
 import express from 'express'
 
 const app = express()
+const server = createServer(app)
+const io = new Server(server)
 
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  res.sendFile(join(__dirname, '/index.html'))
 })
 
-app.listen(process.env.PORT || 3000)
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg)
+  })
+})
+
+const port = process.env.PORT || 3000
+server.listen(port, () => { console.log('listening on :' + port) })
